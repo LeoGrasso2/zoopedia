71bc8878b499c002f58d85a0994a751fbc6c86fd
LeoGrasso2 <grassomleo@gmail.com>
2022-10-24 23:02:37 -0300

    terminado slider y slider inner de imgs

--- js/main.js
+++ js/main.js
@@ -5,27 +5,45 @@ let animales = {
         name: "Vicuña",
         name_sci: "Vicugna vicugna",
         desc: "Camélido de cuello muy largo y de cabeza pequeña. Sus orejas y ojos son prominentes y tiene una hendidura central en el labio superior. Su pelaje es finísimo siendo muy apreciado. Tienen largos y sedosos mechones de color blanco sucio que cuelgan del pecho y lo protegen del frío cuando se echa. El cuello lomo y los lados son de color café claro o canela; el vientre y el interior de los muslos son de color blanco.  Las extremidades son largas y el tercio anterior del cuerpo es más bajo que el posterior.",
-        info: "La vicuña se alimenta casi exclusivamente de pasto, prefiriendo las zonas ricas en agua. Prefieren gramíneas cortas y herbáceas y algunas plantas suculentas; raramente comen pastos duros, aunque sí líquenes.<br><br>El cortejo delas vicuñas es muy primitivo, el macho muy excitado, persigue a la hembra elegida hasta agotarla, la empuja, muerde y da la impresión de que la fuerza a la cópula. La cópula tiene lugar con la hembra en pie o arrodillada y en ocasiones, tumbada. La gestación dura unos 11 meses, las hembras crían todos los años y entran en celo nada mas parir. La única cría es capaz de andar casi nada mas nacer. Al año aproximadamente, los jóvenes machos son expulsados del rebaño y forman grupos de solteros.<br><br>Sus principales enemigos son el puma y el zorro y tras estos el hombre, el cual la ha cazado debido a la suavidad, belleza y valor de su pelo.",
         img: "..\\res\\img\\animals\\noa\\vicuna\\vicuna.png",
         fact1: "Son casi exclusivamente herbívoras; se alimentan de hierbas que cortan con sus dientes sin arrancarlas.",
         fact2: "Tienen una longevidad promedio de unos 15 o 20 años.",
-        fact3: "Su peso es aproximadamente de entre 45 y 55 Kg."
+        fact3: "Su peso es aproximadamente de entre 45 y 55 Kg.",
+        slide: {
+            slide0: "..\\res\\img\\animals\\noa\\vicuna\\0.png",
+            slide1: "..\\res\\img\\animals\\noa\\vicuna\\1.png",
+            slide2: "..\\res\\img\\animals\\noa\\vicuna\\2.png",
+        },
     },
     oso_hormiguero: {
         id:1,
         name: "Oso hormiguero",
         name_sci: "Myrmecophaga tridactyla",
-        desc: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Unde molestias iste delectus rem, doloribus dolore repellat voluptate quas corrupti debitis dolores, voluptates expedita adipisci aut molestiae ad labore exercitationem cumque.",
-        info: "Labore distinctio reprehenderit quod ullam aut nihil aliquam accusantium non? Libero, tempore magnam numquam repellendus impedit quod ab fuga dolores ut ex ipsam, asperiores possimus nesciunt minima modi dolorum! Earum.",
-        img: "..\\res\\img\\animals\\noa\\oso_hormiguero.png",
+        desc: "El oso hormiguero es un animal que suele vivir en zonas de bosques algo achaparrados y sobre todo en llanura. Al ser molestado, emprende un galope bastante ágil, y enfrenta a sus enemigos con sus poderosas garras.<br>Su pelaje es bastante rústico y largo, llegando a medir en el dorso de 15 a 20 cm; alcanza su máxima longitud en la cola, con unos 40 cm de largo, presentando la cerda en esta zona una forma aplanada.<br>Su coloración es gris matizado, con una característica banda negra que atraviesa el pecho, pasando por los flancos hasta la espalda, y bordeada en su límite superior por una franja blanca algo indefinida.",
+        img: "..\\res\\img\\animals\\noa\\oso_hormiguero\\oso_hormiguero.png",
+        fact1: "Se alimenta de hormigas y termitas que captura gracias a su lengua larga y espinosa y a la saliva pegajosa.",
+        fact2: "Viven, en promedio, unos 14 años.",
+        fact3: "Tienen un peso de entre 27 y 41kg.",
+        slide: {
+            slide0: "..\\res\\img\\animals\\noa\\oso_hormiguero\\0.png",
+            slide1: "..\\res\\img\\animals\\noa\\oso_hormiguero\\1.png",
+            slide2: "..\\res\\img\\animals\\noa\\oso_hormiguero\\2.png",
+        },
         },
     condor_andino: {
         id:2,
         name: "Condor andino",
         name_sci: "Vultur gryphus",
-        desc: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Unde molestias iste delectus rem, doloribus dolore repellat voluptate quas corrupti debitis dolores, voluptates expedita adipisci aut molestiae ad labore exercitationem cumque.",
-        info: "Labore distinctio reprehenderit quod ullam aut nihil aliquam accusantium non? Libero, tempore magnam numquam repellendus impedit quod ab fuga dolores ut ex ipsam, asperiores possimus nesciunt minima modi dolorum! Earum.",
-        img: "..\\res\\img\\animals\\noa\\condor_andino.png",
+        desc: "El cóndor de los Andes es una de las especies rapaces más grandes del neotrópico. El largo del cuerpo es de 120 cm, alcanzando una envergadura de 300 cm y un peso de alrededor de 12 kg. Su pico es blanco-crema en el extremo y negro en la base. Tiene patas grises y el iris marrón amarillento. El macho tiene el cuello y la cabeza desnudos y rojizos. La cabeza presenta una carúncula alargada a manera de cresta carnosa que parte de la cera hasta la mitad de la cabeza. Su collar es blanco vistoso en la base del cuello, siendo el resto de su cuerpo negro.<br>Cuenta con una amplia zona blanca en el ala dorsal y su cola es de color negro. La hembra es similar al macho, pero de menor tamaño y sin cresta o carúncula en la cabeza. El iris es rojo. El cóndor juvenil es pardo oscuro y lleva collar del mismo color, sin cresta ni blanco en las alas..",
+        img: "..\\res\\img\\animals\\noa\\condor_andino\\condor_andino.png",
+        fact1: "Los cóndores son buitres; los cadáveres de animales grandes componen la mayor parte de su dieta",
+        fact2: "Pueden llegar a vivir 80 años, aunque el promedio es unos 50.",
+        fact3: "Tienen un peso de alrededor de 12 kg.",
+        slide: {
+            slide0: "..\\res\\img\\animals\\noa\\condor_andino\\0.png",
+            slide1: "..\\res\\img\\animals\\noa\\condor_andino\\1.png",
+            slide2: "..\\res\\img\\animals\\noa\\condor_andino\\2.png",
+        },
         }
     }
 }
@@ -102,24 +120,56 @@ function addCard(datos){
                                     ${datos.desc}
                                 </div>
                                 <hr>
-                                <div class="card-right-img-slider>
-                                    <div class="card-right-img-slider-wrapper>
-                                    <div class="card-right-img-slider-element"">
-                                        <img id="img-el0">
-                                    </div>
-                                    <div class="card-right-img-slider-element" id="el1">
-                                        <img id="img-el1">
-                                    </div>
-                                    <div class="card-right-img-slider-element" id="el2">
-                                        <img id="img-el2">
-                                    </div>
+                                <div class="card-right-img-slider">
+                                    <div class="card-right-img-slider-prevBtn" id="${datos.id}-prevBtn"><</div>
+                                    <div class="card-right-img-slider-container">
+                                    <div class="card-right-img-slider-wrapper">
+                                        <div class="card-right-img-slider-element" id="el0">
+                                            <img id="img-${datos.id}-el0">
+                                        </div>
+                                        <div class="card-right-img-slider-element" id="el1">
+                                            <img id="img-${datos.id}-el1">
+                                        </div>
+                                        <div class="card-right-img-slider-element" id="el2">
+                                            <img id="img-${datos.id}-el2">
+                                        </div>
                                 </div>
+                                </div>
+                                    <div class="card-right-img-slider-prevBtn" id="${datos.id}-nextBtn">></div>
                         </div>
                     </div>`;
+
     body.appendChild(card);
+    let prevBtn = document.getElementById(`${datos.id}-prevBtn`);
+    let nextBtn = document.getElementById(`${datos.id}-nextBtn`);
+    let transVal=0;
     let imgAnimal =  document.getElementById(`imgAnimal${datos.id}`);
-    let img_el0 =  document.getElementById(`img-el${datos.slide0}`);
-    let img_el1 =  document.getElementById(`img-el${datos.slide1}`);
-    let img_el2 =  document.getElementById(`img-el${datos.slide2}`);
+    let i=0;
+    for(img of Object.keys(datos.slide)){
+        console.log(img)
+        let img_el =  document.getElementById(`img-${datos.id}-el${i}`);
+        img_el.setAttribute('src', datos.slide[img])
+        i++;
+    }
+    
+    prevBtn.addEventListener('click', () => {
+        let card = document.getElementsByClassName('card-right-img-slider-wrapper')[datos.id];
+        transVal +=500; 
+        if(transVal>=500){
+            transVal= (-1000);
+        }       
+        card.style.transform = `translateX(${transVal}px)`
+        card.style.transition = `.5s`;
+    })
+
+    nextBtn.addEventListener('click', () => {
+        let card = document.getElementsByClassName('card-right-img-slider-wrapper')[datos.id];
+        transVal -=500; 
+        if(transVal<=(-1500)){
+            transVal= 0;
+        }     
+        card.style.transform = `translateX(${transVal}px)`
+        card.style.transition = `.5s`;
+    })
     imgAnimal.setAttribute('src', `${datos.img}`)
 }
