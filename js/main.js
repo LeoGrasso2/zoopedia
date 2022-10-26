78f070c72af09f39a93508e005421879e2c02c0e
LeoGrasso2 <grassomleo@gmail.com>
2022-10-26 00:27:26 -0300

    terminado NOA, comenzado chaco 1/5

--- js/main.js
+++ js/main.js
@@ -51,9 +51,58 @@ let animales = {
         name_sci: "Hippocamelus bisulcus",
         desc:"El huemul, como su pariente filogenético, la taruca, tiene un cuerpo rechoncho, robusto y patas cortas. Alcanza un tamaño de hasta 165 cm de longitud y hasta 170 cm de altura contando las astas, siendo las hembras un poco más pequeñas que los machos. Su pelaje es grueso, denso y de color beige o café oscuro, según la época del año.<br>Sus orejas y su cola, miden entre 10 y 20 cm de largo. Los machos poseen un par de astas bifurcadas que pueden alcanzar hasta 30 cm de longitud. Su peso ronda entre los 40 y 100 kg. que se alimenta principalmente de arbustos, hierbas y brotes de árboles",
         img:"..\\res\\img\\animals\\noa\\huemul\\huemul.png",
-        fact1: "Se alimenta principalmente de arbustos, hierbas y brotes de árboles."
-    }
-    }
+        fact1: "Se alimenta principalmente de arbustos, hierbas y brotes de árboles.",
+        fact2: "Viven aproximadamente unos 20 años.",
+        fact3: "Las hembras pesan unos 65 kg, mientras que los machos pueden llegar a superar los 90 kg.",
+        slide: {
+            slide0: "..\\res\\img\\animals\\noa\\huemul\\0.png",
+            slide1: "..\\res\\img\\animals\\noa\\huemul\\1.png",
+            slide2: "..\\res\\img\\animals\\noa\\huemul\\2.png",
+        }
+        },
+    flamenco_austral: {
+        id: 4,
+        name: "Flamenco austral",
+        name_sci: "Phoenicopterus chilensis",
+        desc: "Posee una altura de 1,10 a 1,30 m. De plumaje rosado claro (salmón), presenta algunas zonas más oscuras cerca de la cola. El pico es grande, encorvado hacia abajo, de color claro en la parte más cercana a la cabeza y negro en la parte más extrema. Las piernas son claras, a excepción de la articulación del tarso (mal llamada rodilla), que es de un rosado intenso. Los ojos son de color amarillo.<br>El color rosado en los flamencos proviene de los carotenoides, los pigmentos que poseen las algas y crustáceos, alimento por excelencia de los flamencos.",
+        img: "..\\res\\img\\animals\\noa\\flamenco_austral\\flamenco_austral.png",
+        fact1: "Se alimenta de pequeños crustáceos, moluscos y algas filtrándolos con su pico.",
+        fact2: "Su promedio de vida es de unos 40 años.",
+        fact3: "Tienen un peso de 1,7 a 2,5 kg.",
+        slide: {
+            slide0: "..\\res\\img\\animals\\noa\\flamenco_austral\\0.png",
+            slide1: "..\\res\\img\\animals\\noa\\flamenco_austral\\1.png",
+            slide3: "..\\res\\img\\animals\\noa\\flamenco_austral\\2.png",
+        }
+        },
+    },
+    chaco:{
+    nandu: {
+        id: 0,
+        name: "Ñandú",
+        name_sci: "Rhea americana",
+        desc: "El macho alcanza una altura de 1,50 m, y la hembra 1,20 m.<br>Las patas, desprovistas de plumas, están desarrolladas y adaptadas para la carrera, y terminan en tres dedos, con una robusta uña comprimida lateralmente en cada dedo. El cuello es largo y las alas inútiles para el vuelo, solo utilizadas para dar equilibrio al cuerpo durante el giro en la carrera. La cabeza, pequeña y de color ceniciento, está dotada de un pico fuerte, del mismo tamaño que la cabeza, deprimido y ancho en la base. En la base del cuello, en el pecho y en la parte superior de la cabeza la coloración es negra, siendo en el macho más destacada esta coloración negra en las partes mencionadas, mientras que el resto del cuerpo es gris.<br>Alrededor de los ojos y oídos no posee plumas, sino que están rodeados estos por piel rugosa.",
+        img: "..\\res\\img\\animals\\chaco\\nandu\\nandu.png",
+        fact1: "Su dieta consiste en hierba, semillas, frutos, insectos, reptiles e incluso pequeños mamíferos.",
+        fact2: "En condiciones naturales, pueden vivir hasta 15 años.",
+        fact3: "El peso es de aproximadamente 30 a 35 kg en el macho y alrededor de los 25 kg en la hembra",
+        slide: {
+            slide0: "..\\res\\img\\animals\\chaco\\nandu\\0.png",
+            slide1: "..\\res\\img\\animals\\chaco\\nandu\\1.png",
+            slide3: "..\\res\\img\\animals\\chaco\\nandu\\2.png",
+        },
+    
+    },
+    },
+}
+
+let regiones= ['chaco', 'cuyo', 'mesopotamia', 'noa', 'pampaHumeda', 'pampaSeca', 'patagonia', 'sierrasPampeanas'];
+let pagActual = (location.href.split("/").slice(-1))[0];
+pagActual = pagActual.slice(0, pagActual.length - 5);
+console.log(pagActual);
+
+if(regiones.includes(pagActual) == true){
+    slider(pagActual)
 }
 
 function slider(region){
@@ -74,8 +123,9 @@ function slider(region){
             let cardsDiv = document.getElementsByClassName('animalCards')[0];
             transVal +=1000; 
             if(transVal>=1000){
-                transVal= (-2000);
+                transVal= (-4000);
             }       
+            console.log(transVal)
             cardsDiv.style.transform = `translateX(${transVal}px)`
             cardsDiv.style.transition = `.5s`;
         })
@@ -83,20 +133,15 @@ function slider(region){
         next.addEventListener('click', () => {
             let cardsDiv = document.getElementsByClassName('animalCards')[0];
             transVal -=1000; 
-            if(transVal<=(-2400)){
+            if(transVal<(-4000)){
                 transVal= 0;
             }     
+            console.log(transVal)
             cardsDiv.style.transform = `translateX(${transVal}px)`
             cardsDiv.style.transition = `.5s`;
         })
     }
 }
-
-
-document.addEventListener('DOMContentLoaded', function(event) {
-    document.querySelector('body').style.opacity = 1;
-})
-
 
 function addCard(datos){
     let card = document.createElement('div');
